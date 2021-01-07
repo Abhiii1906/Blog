ac1d51bc4c0e4a1c608ed5716ad9a249c6efa60d
Abhishek Yadav <abhisheky414@gmail.com>
2021-01-07 19:15:23 +0530

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('', include('blog.urls', namespace='blog')),
 ]
